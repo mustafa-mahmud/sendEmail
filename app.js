bafef19e08cee11d781b6a4af7b78e10dbd7c167
mithu <mithu@gmail.com>
2020-09-03 13:38:46 +0600

    add files

---
+++ app.js
@@ -0,0 +1,198 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+	const btn = document.querySelector("button");
+	const courseName = "PHP OPERATOR";
+
+	btn.addEventListener("click", sendData);
+
+	async function sendData() {
+		//get q body
+		const qBodyRes = await fetch("./qBody.xml");
+		const qBodyData = await qBodyRes.text();
+		const qBodyDataProcess = qBodyData.split("newPart");
+		qBodyDataProcess.shift();
+
+		for (let i = 0; i < qBodyDataProcess.length; i++) {
+			qBodyDataProcess[i] = qBodyDataProcess[i].replace(/\r\n/gm, "").trim();
+		}
+
+		//get q
+		const qRes = await fetch("./q.xml");
+		const qData = await qRes.text();
+		const qDataProcess = qData.split("newQ");
+		qDataProcess.shift();
+
+		for (let i = 0; i < qDataProcess.length; i++) {
+			qDataProcess[i] = qDataProcess[i].trim().replace(/\r\n/gm, "")
+		}
+
+		//get ans
+		const ansRes = await fetch("./ans.xml");
+		const ansData = await ansRes.text();
+		const ansDataProcess = ansData.split("newAns");
+		ansDataProcess.shift();
+
+		for (let i = 0; i < ansDataProcess.length; i++) {
+			ansDataProcess[i] = ansDataProcess[i].replace(/\r\n/gm, "").trim();
+		}
+
+		//get LS
+		const userAns = [17, "*,/,+,-", "false", "false", 10, 99, "true", "no", "", 25];
+		localStorage.setItem("userAns", JSON.stringify(userAns));
+		const parseUserAns = JSON.parse(localStorage.getItem("userAns"));
+
+		const jsonData = [
+			[qBodyDataProcess, qDataProcess, ansDataProcess, parseUserAns, courseName]
+		]
+
+		//console.log(jsonData);
+
+		//see question body
+		for (let i = 0; i < jsonData.length; i++) {
+			for (let j = 0; j < jsonData[0][0].length; j++) {
+				//console.log(jsonData[0][0][j]);
+			}
+		}
+
+		//see question
+		for (let i = 0; i < jsonData.length; i++) {
+			for (let j = 0; j < jsonData[0][1].length; j++) {
+				//console.log(jsonData[0][1][j]);
+			}
+		}
+
+		//see correct answer
+		for (let i = 0; i < jsonData.length; i++) {
+			for (let j = 0; j < jsonData[0][2].length; j++) {
+				//console.log(jsonData[0][2][j]);
+			}
+		}
+
+		//see user given anser
+		for (let i = 0; i < jsonData.length; i++) {
+			for (let j = 0; j < jsonData[0][3].length; j++) {
+				//console.log(jsonData[0][3][j]);
+			}
+		}
+
+		let qBodyHtml = jsonData[0][0].map((item, index) => {
+			return `
+					<div class='wrapper' style='padding: 20px;'>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td align='center'
+											style='color: #fff;font-size: 30px;display: inline-block;padding: 5px 10px;background-color: #0E6251;'>
+											${index + 1}
+											</td>
+										<td width='30' align='center' height='30' colspan='1'></td>
+
+										<td align='left'>
+											<code style='font-size: 16px;'>
+											${item}
+										</code>
+										</td>
+									</tr>
+								</table>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td width='600' align='center' height='30'></td>
+									</tr>
+								</table>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td align='left'>
+											<span style='font-weight: bold; font-size: 20px;'>Question Was:</span>
+											<span style='font-size: 18px;'>${jsonData[0][1][index]}</span>
+										</td>
+									</tr>
+								</table>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td width='600' align='center' height='10'></td>
+									</tr>
+								</table>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td align='left'>
+											<span style='font-weight: bold;font-size: 20px;'>Your Answer Was:</span>
+											<span
+											style = 'font-weight:bold ;font-size: 18px; ${(jsonData[0][2][index] != jsonData[0][3][index]) ? 'color: #FF0000 ' : 'color: #32CD32'}'>
+											${jsonData[0][3][index]}
+											</span>
+										</td>
+									</tr>
+								</table>
+
+								<table table border = '0'	cellpadding = '0'	cellspacing = '0'	width = '100%'
+								style = '${(jsonData[0][2][index] != jsonData[0][3][index]) ? 'display:block' : 'display:none'}'
+								>
+									<tr>
+										<td align='left'>
+											<span style='font-weight: bold;font-size: 20px;'>Correct Answer Was:</span>
+											<span style='font-size: 18px; color:#32CD32;font-weight:bold'>${(jsonData[0][2][index]!=jsonData[0][3][index])?jsonData[0][2][index]:""}</span>
+										</td>
+									</tr>
+								</table>
+
+								<table border='0' cellpadding='0' cellspacing='0' width='100%'>
+									<tr>
+										<td width='600' align='center' height='10' style='border-bottom: 1px dotted #666;'></td>
+									</tr>
+								</table>
+							</div>
+			`;
+		});
+
+		console.log(qBodyHtml);
+
+		let output = `
+			<table bgcolor='#666666' width='100%' align='center' border='0' cellspacing='0' cellpadding='0'
+		style='padding: 30px 0px;'>
+		<tr>
+			<td align='center'>
+				<table bgcolor='#F6F6F6' align='center' border='0' cellpadding='0' cellspacing='0'>
+					<tr>
+						<td width='600' align='center' style='padding: 20px 10px;'>
+							<table border='0' cellpadding='0' cellspacing='0' style='display: inline-block;'>
+								<tr>
+									<td width='600' align='center'
+										style='font-size: 30px;font-style: italic;font-weight: bold;border-bottom: 2px solid #222;'>
+										${jsonData[0][4]}
+									</td>
+								</tr>
+								<tr>
+									<td width='600' align='center' height='30' colspan='1'></td>
+								</tr>
+							</table>
+
+							${qBodyHtml}
+
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+		`;
+
+		const show = document.querySelector(".show");
+		show.innerHTML = output;
+
+		const res = await fetch("./mail.php", {
+			method: "post",
+			body: output,
+			headers: {
+				"Content-Type": "application/text"
+			}
+		});
+		const resData = await res.text();
+		console.log(resData);
+		//console.log(JSON.parse(resData));
+	}
+
+});
---
+++ mail.php
@@ -0,0 +1,62 @@
+<?php
+//LINK:: https: //github.com/PHPMailer/PHPMailer
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$json = file_get_contents("php://input");
+//$data = json_decode($json, true);
+
+//Get Values from JS file
+//$body = $data[0][4] . $data[0][0][1];
+
+$x = "Hello World";
+
+$body = $json;
+
+$to = "forallweb000@gmail.com";
+$to2 = "mithuweb000@gmail.com";
+$subject = "Check Email Template";
+
+//Get require file
+require_once "PHPMailer/PHPMailer.php";
+require_once "PHPMailer/SMTP.php";
+require_once "PHPMailer/Exception.php";
+
+//Initiate PHPMailer Class
+$mail = new PHPMailer();
+
+//Smtp Settings
+$mail->isSMTP();
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = true;
+$mail->Username = $to;
+$mail->Password = "forall000BD00@#$%Mt";
+$mail->Port = 465; //587
+$mail->SMTPSecure = "ssl";
+
+//Recipients
+$mail->setFrom($to, "Mithu Khan");
+$mail->addAddress($to);
+$mail->addAddress($to2);
+//$mail->addCC('cc@example.com'); //CC
+//$mail->addBCC('bcc@example.com'); //BCC
+
+//Contents
+$mail->isHTML(true);
+$mail->Subject = $subject;
+$mail->Body = $body;
+$mail->AltBody = $body;
+
+//Proccess Error
+if ($mail->send()) {
+  $status = "Success";
+  $response = "Email is sent";
+} else {
+  $status = "Failed";
+  $response = "Something went wrong. " . $mail->ErrorInfo;
+}
+
+exit(json_encode(
+  ["status" => $status, "response" => $response]
+));
+?>
